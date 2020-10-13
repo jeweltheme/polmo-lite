6793d24524356e63cda4149875cd28a3d8c84596
litonarefin <litonice09@gmail.com>
2020-10-13 18:13:11 +0600

    index fixing

--- index.php
+++ index.php
@@ -23,26 +23,24 @@ $layout = polmo_lite_blog_layout();
 		<?php } ?>	
 		
 
-			<?php if ( have_posts() ) {?>
-				<div class="row">
-			 		<?php while ( have_posts() ) { the_post(); 
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-							get_template_part( 'template-parts/content', get_post_format() );
-						}
+		<?php if ( have_posts() ) {?>
 
-						echo function_exists('polmo_lite_pagination') ? polmo_lite_pagination() : posts_nav_link();
+			<div class="row">
+		 		<?php while ( have_posts() ) { the_post(); 
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					}
+					echo function_exists('polmo_lite_pagination') ? polmo_lite_pagination() : posts_nav_link();
+				?>
+			</div>
 
-					?>
-				</div>
-
-				<?php } else { 
-					get_template_part( 'template-parts/content', 'none' ); 
-				}
-			?>
-			
+		<?php } else { 
+			get_template_part( 'template-parts/content', 'none' ); 
+		}?>
+		
 
 		<?php if(($layout['type'] != "layout-grid") || ($layout['type'] != "layout-two-columns")){?>
 			<!-- </div> -->
